cc481dd196850e0f380d1a417b37b6d78490ee3e
Taiwo Abiodun <taiwobiodun@gmail.com>
2023-09-11 19:05:00 +0100

    adding different user routes comments

--- server/routes/users.js
+++ server/routes/users.js
@@ -1,12 +1,14 @@
 const router = require("express").Router();
 let User = require("../models/user.model");
 
+//General users request
 router.route("/").get((req, res) => {
   User.find()
     .then((users) => res.json(users))
     .catch((err) => res.status(400).json("Error: " + err));
 });
 
+//create new user
 router.route("/add").post((req, res) => {
   const username = req.body.username;
   const name = req.body.name;
@@ -21,18 +23,21 @@ router.route("/add").post((req, res) => {
     .catch((err) => res.status(400).json("Error: " + err));
 });
 
+//get user by id
 router.route("/:id").get((req, res) => {
   User.findById(req.params.id)
     .then((users) => res.json(users))
     .catch((err) => res.status(400).json("Error: " + err));
 });
 
+//delete user by id
 router.route("/:id").delete((req, res) => {
   User.findByIdAndDelete(req.params.id)
     .then(() => res.json("User deleted!"))
     .catch((err) => res.status(400).json("Error: " + err));
 });
 
+//update user by id
 router.route("/update/:id").post((req, res) => {
   User.findById(req.params.id)
     .then((user) => {
