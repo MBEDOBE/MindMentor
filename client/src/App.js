6b511b6dbf092819a324cf8d50c992ef08cf54ad
Taiwo Abiodun <taiwobiodun@gmail.com>
2023-09-14 01:00:35 +0100

    Setup browser view and install axios

--- client/src/App.js
+++ client/src/App.js
@@ -1,10 +1,11 @@
-import { createBrowserRouter, RouterProvider, Outlet } from 'react-router-dom';
-import Login from './pages/auth/Login';
-import Register from './pages/auth/Register';
-import Navbar from './components/Navbar';
-import Footer from './components/Footer';
-import Landing from './pages/home/Landing';
-import './App.css';
+import { createBrowserRouter, RouterProvider, Outlet } from "react-router-dom";
+import Login from "./pages/auth/Login";
+import Register from "./pages/auth/Register";
+import Navbar from "./components/Navbar";
+import Footer from "./components/Footer";
+import Landing from "./pages/home/Landing";
+import "./App.css";
+import React from "react";
 
 const Layout = () => {
   return (
@@ -18,22 +19,21 @@ const Layout = () => {
 
 const router = createBrowserRouter([
   {
-    path: '/',
+    path: "/",
     element: <Layout />,
     children: [
       {
-        path: '/',
+        path: "/",
         element: <Landing />,
       },
-      
     ],
   },
   {
-    path: '/register',
+    path: "/register",
     element: <Register />,
   },
   {
-    path: '/login',
+    path: "/login",
     element: <Login />,
   },
 ]);
